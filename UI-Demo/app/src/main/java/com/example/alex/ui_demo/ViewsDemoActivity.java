a6975242bc0c497cefd1dafadc4c48c4b1d3723f
Alex <alex.neher@stud.hslu.ch>
2018-03-11 02:10:46 +0100

    Kinda finished 2.4. GIF-playing aint working...

--- UI-Demo/app/src/main/java/com/example/alex/ui_demo/ViewsDemoActivity.java
+++ UI-Demo/app/src/main/java/com/example/alex/ui_demo/ViewsDemoActivity.java
@@ -2,12 +2,33 @@ package com.example.alex.ui_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ViewsDemoActivity extends AppCompatActivity {
+
+    RatingBar ratingBar;
+    TextView ratingTextView;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_views_demo);
+        ratingBar = (RatingBar) findViewById(R.id.ratingBar);
+        ratingTextView = (TextView) findViewById(R.id.stars);
+        webView = (WebView) findViewById(R.id.webView);
+
+        String source = "<!DOCTYPE html><html><body><img src=\"https://media.giphy.com/media/QbumCX9HFFDQA/giphy.gif\" alt=\"Hackerman\" width=\"100%\" height=\"100%\"></body></html>";
+        webView.loadData(source, "text/html", "utf-8");
+
+    }
+
+    public void updateRating(View view){
+        float rating = ratingBar.getRating();
+        String ratingString = Float.toString(rating);
+        ratingTextView.setText(ratingString);
     }
 }
