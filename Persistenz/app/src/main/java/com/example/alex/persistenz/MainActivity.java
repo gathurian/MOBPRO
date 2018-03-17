b31d6ff0fdd6c213e2f9b930d54577cef0b01757
Alex <alex.neher@stud.hslu.ch>
2018-03-17 13:51:43 +0100

    Added Persistenz-App

---
+++ Persistenz/app/src/main/java/com/example/alex/persistenz/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.alex.persistenz;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView onResume_text;
+    String onResumeCounter = "0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        onResume_text = (TextView) findViewById(R.id.preference_text);
+
+        final SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        final int newResumeCount = preferences.getInt("COUNTER_KEY", 0)+1;
+        editor.putInt("COUNTER_KEY", newResumeCount);
+        editor.apply();
+
+        onResume_text.setText("MainActivity.onResume() wurde seit der Installation dieser App " + newResumeCount + " mal aufgerufen");
+    }
+}
