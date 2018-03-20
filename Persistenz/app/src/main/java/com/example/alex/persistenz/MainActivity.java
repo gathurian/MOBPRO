e1dee134ddcb17eab4a7abfc80e805cc31e84339
alex <alex.neher@stud.hslu.ch>
2018-03-20 12:45:35 +0100

    Added saving and loading notes from internal and external storage

--- Persistenz/app/src/main/java/com/example/alex/persistenz/MainActivity.java
+++ Persistenz/app/src/main/java/com/example/alex/persistenz/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.alex.persistenz;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Environment;
 import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.PreferenceManager;
@@ -9,7 +13,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,8 +78,116 @@ public class MainActivity extends AppCompatActivity {
     private void setTeaPrefText(){
         SharedPreferences teaPrefs = PreferenceManager.getDefaultSharedPreferences(this);
         String teaPreferences = teaPrefs.getString("teaPreferred", "ohne nix");
-        String sweetener = teaPrefs.getString("teaSweetener", "keinem Süssungsmittel");
-        tea_preference.setText("ich trinke am liebsten " + teaPreferences + " mit " + sweetener);
+        Boolean teaSweetened = teaPrefs.getBoolean("sweetTea", false);
+        String sweetener;
+        if(teaSweetened) {
+            sweetener = " mit " + teaPrefs.getString("teaSweetener", "keinem Süssungsmittel");
+        } else{
+            sweetener = " ungesüsst";
+        }
+        tea_preference.setText("ich trinke am liebsten " + teaPreferences + sweetener);
+    }
+
+    public void saveData(View Button){
+        CheckBox sdCard = (CheckBox) findViewById(R.id.writeToSDCard);
+        EditText data = (EditText) findViewById(R.id.editText);
+        if(data.getText().toString().length() == 0){
+            Toast.makeText(getApplicationContext(), "You haven't written anything yet", Toast.LENGTH_SHORT).show();
+        } else {
+            if (sdCard.isChecked()) {
+                boolean ExternalStorageAvailable = false;
+                boolean ExternalStorageWritable = false;
+                String state = Environment.getExternalStorageState();
+
+                if (Environment.MEDIA_MOUNTED.equals(state)) {    //Check if SD-Card is mounted
+                    ExternalStorageAvailable = ExternalStorageWritable = true;
+                } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {       //Check if SD-Card is writable
+                    ExternalStorageWritable = false;
+                    ExternalStorageAvailable = true;
+                } else {
+                    ExternalStorageWritable = ExternalStorageAvailable = false;
+                }
+
+                if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    File directory = new File(android.os.Environment.getExternalStorageDirectory().getAbsolutePath() + "/persistence");
+                    directory.mkdirs();
+                    File notes = new File(directory, "notes.txt");
+                    if (ExternalStorageAvailable == ExternalStorageWritable == true) {
+                        try {
+                            FileOutputStream fOut = new FileOutputStream(notes);
+                            PrintWriter pw = new PrintWriter(fOut);
+                            pw.print(data.getText().toString());
+                            pw.flush();
+                            pw.close();
+                            fOut.close();
+                            Toast.makeText(getApplicationContext(), "file saved", Toast.LENGTH_SHORT).show();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Could not write to SD-Card. Is it mounted?", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},23);
+                }
+            } else {
+                try {
+                    FileOutputStream fOut = openFileOutput("notes.txt", MODE_PRIVATE);
+                    String note = data.getText().toString();
+                    fOut.write(note.getBytes());
+                    fOut.close();
+                    Toast.makeText(getApplicationContext(), "file saved", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+    }
+
+    public void loadData(View Button){
+        CheckBox sdCard = (CheckBox) findViewById(R.id.writeToSDCard);
+        EditText data = (EditText) findViewById(R.id.editText);
+        data.setText("");
+
+        if(sdCard.isChecked()){
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    final File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/persistence", "notes.txt");
+                    FileInputStream fis = new FileInputStream(file);
+                    InputStreamReader is = new InputStreamReader(fis);
+                    BufferedReader br = new BufferedReader(is);
+                    StringBuilder sb = new StringBuilder();
+                    String stLine;
+                    while ((stLine = br.readLine()) != null) {
+                        sb.append(stLine);
+                    }
+                    data.setText(sb.toString());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else{
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},24);
+            }
+
+        } else {
+            try {
+                FileInputStream fis = getApplicationContext().openFileInput("notes.txt");
+                InputStreamReader is = new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(is);
+                StringBuilder sb = new StringBuilder();
+                String string;
+                while ((string = br.readLine()) != null) {
+                    sb.append(string);
+                }
+                data.setText(sb.toString());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
