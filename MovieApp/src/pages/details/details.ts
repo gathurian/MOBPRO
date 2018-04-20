e18f73686e7c44f51643ca6960dc09353e2e7088
alex <alex.neher@stud.hslu.ch>
2018-04-20 15:24:40 +0200

    Added MovieApp

---
+++ MovieApp/src/pages/details/details.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-details',
+  templateUrl: 'details.html'
+})
+export class DetailPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
